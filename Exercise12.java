80b0ee65d18eba10399af589a604d031c916fae5
Alexei Sharipov <xviiixxcvi1810@gmail.com>
2019-09-12 18:51:58 +0300

    Ex12 new

--- Exercise12.java
+++ Exercise12.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 public class Exercise12 {
     public static void main(String[] args) {
-        System.out.println("vvedite chislo do kotorogo budet idti proverka");
+        System.out.println("vvedite kol-vo kratnych 3 chisel");
         Scanner sc=new Scanner(System.in);
-        int sum=0;
         int n=sc.nextInt();
-        for( int a=1; a<=n; a++){
+        int board=n*3;
+        int sum=0;
+        for( int a=1; a<=board; a++){
             int dig=a%3;
             if(dig==0){
                 sum+=a;
             }
-            if (a==n){
-                System.out.println("summa nuzhnyh chisel="+sum);
             }
+        System.out.println("summa="+sum);
         }
     }
-}
